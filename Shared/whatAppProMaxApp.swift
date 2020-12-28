3263bee6404448be2598987a87f0179fd1876bb0
frimpongopoku <pongofrimi@gmail.com>
2020-12-28 12:03:03 +0000

    Initial Commit

---
+++ Shared/whatAppProMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatAppProMaxApp.swift
+//  Shared
+//
+//  Created by frimpongOpokuAgyemang on 28/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct whatAppProMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
